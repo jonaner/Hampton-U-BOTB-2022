954c93f992df289381ba185f103ededd12c7d594
Anthony Odum <grandmastervirg@Anthonys-MacBook-Air-2.local>
2022-03-11 02:57:39 -0500

    pages added

---
+++ hu-botb/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAzUy7SRbq69U3jw23PqwE3srSyuh9RLQ",
+  authDomain: "hu-botb.firebaseapp.com",
+  projectId: "hu-botb",
+  storageBucket: "hu-botb.appspot.com",
+  messagingSenderId: "809601251887",
+  appId: "1:809601251887:web:f7cebc43c12e54c959a4da"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
